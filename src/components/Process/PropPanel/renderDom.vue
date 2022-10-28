6c827b1d8d6571e0798c180a646a93e7867c7b88
susheng <lisusheng@dycjr.com>
2022-10-28 18:50:17 +0800

    add: 增加VNode渲染组件

---
+++ src/components/Process/PropPanel/renderDom.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    name: "renderDom",
+    functional: true,
+    props: {
+        render: Function,
+    },
+    render: (h, ctx) => {
+        return ctx.props.render(h);
+    }
+}
+</script>
